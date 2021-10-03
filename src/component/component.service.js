14b11911b3b33a71ba7dabe56a7a5329a9fd18bc
georgiosan <georgiosanagnostou@hotmail.com>
2021-10-03 23:19:52 +0300

    Added the logic behind the component class, along with several core methods

---
+++ src/component/component.service.js
@@ -0,0 +1,45 @@
+import removeElement from "../utils/delete.element.js";
+import enable from "../init/check.service.js";
+
+export default class Component {
+  static count = 0;
+  constructor(title, description, date, priority) {
+    this.id = Component.count++;
+    this.title = title;
+    this.description = description;
+    this.date = date;
+    this.priority = priority;
+    this.state = "active";
+  }
+  static resetCount() {
+    Component.count = 0;
+  }
+  get keys() {
+    return Object.keys(this);
+  }
+  render() {
+    const sectionWrapper = document.querySelector(".main-section");
+    const template = document.querySelector("template");
+    const clone = template.content.cloneNode(true);
+    const parentElement = clone.childNodes[0].nextSibling;
+    const componentTitle = clone.querySelector(".header-wrapper h3");
+    const componentDescription = clone.querySelector(".info-desc p");
+    const componentDate = clone.querySelector(".d-wrap p");
+    const componentPriority = clone.querySelector(".prio-wrap p");
+    [
+      componentTitle.textContent,
+      componentDescription.textContent,
+      componentDate.textContent,
+      componentPriority.textContent,
+    ] = [this.title, this.description, this.date, this.priority];
+    parentElement.classList.add(`item${this.id}`);
+    sectionWrapper.appendChild(clone);
+  }
+  remove() {
+    removeElement(this.id);
+    this.keys.forEach((key) => delete this[key]);
+  }
+  enableButton() {
+    enable(this.id);
+  }
+}
