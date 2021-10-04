13b94666df5e30959066ccde09f3f93400624f34
georgiosan <georgiosanagnostou@hotmail.com>
2021-10-05 01:26:04 +0300

    Added clearDOM func to clear all section nodes

--- src/utils/delete.element.js
+++ src/utils/delete.element.js
@@ -2,3 +2,10 @@ export default function removeElement(id) {
   const target = document.querySelector(`.item${id}`);
   target.remove();
 }
+function clearDOM() {
+  const mainSection = document.querySelector(".main-section");
+  while (mainSection.firstChild) {
+    mainSection.removeChild(mainSection.firstChild);
+  }
+}
+export { clearDOM };
