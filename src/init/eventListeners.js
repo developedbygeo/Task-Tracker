ea1a80e43a2fc5c3092ba4600ece9976b060bdbb
georgiosan <georgiosanagnostou@hotmail.com>
2021-10-06 20:34:56 +0300

    Added the toggleWarning function which is invoked when the clear btn is clicked

--- src/init/eventListeners.js
+++ src/init/eventListeners.js
@@ -10,14 +10,24 @@ import {
 import create from "/src/utils/create.element.js";
 import { clearDOM } from "../utils/delete.element.js";
 import getLocalStorageKeys from "../utils/localStorage.checker.js";
+import {
+  deleteFromLocalStorage,
+  clearKey,
+} from "../utils/localStorage.controller.js";
+import { cacheLoad, cacheSearch } from "../search/cache.js";
+import searchResult from "../search/search.js";
 
 export default function enableListeners() {
+  const search = document.querySelector("#search");
   const toggleBtn = document.querySelector(".toggle");
   const closeBtn = document.querySelector(".close");
   const menuBtn = document.querySelector(".add");
   const cancelBtn = document.querySelector(".decline");
   const acceptBtn = document.querySelector(".accept");
   const addProjectBtn = document.querySelector(".add-new-list");
+  const clearProjectBtn = document.querySelector(".clear");
+  const confirmDelete = document.querySelector(".warn-confirm");
+  const declineDelete = document.querySelector(".warn-decline");
 
   toggleBtn.addEventListener("click", toggleSideBar);
   closeBtn.addEventListener("click", toggleSideBar);
@@ -27,6 +37,18 @@ export default function enableListeners() {
   addProjectBtn.addEventListener("click", addProject);
   window.addEventListener("load", projectInit, true);
   window.addEventListener("load", sideNavInit);
+  window.addEventListener("load", cacheLoad);
+  confirmDelete.addEventListener("click", deleteProject);
+  clearProjectBtn.addEventListener("click", toggleWarning);
+  declineDelete.addEventListener("click", toggleWarning);
+  search.addEventListener("change", query);
+}
+
+function query() {
+  const queryString = document.querySelector("#search");
+  const search = cacheSearch(queryString.value);
+  clear(queryString);
+  searchResult(search);
 }
 
 function toggleSideBar() {
@@ -40,7 +62,10 @@ function toggleMenu() {
 // activates the toggle-active button for each rendered object
 export function buttonActive() {
   const parent = this.offsetParent;
+  const title = this.offsetParent.children[1].children[0].textContent;
+  const currentProject = document.querySelector(".current").textContent;
   parent.classList.toggle("check-active");
+  deleteFromLocalStorage(currentProject, title);
 }
 // creates new Component instance with the details provided through the menu-add
 function addNewTask() {
@@ -71,6 +96,17 @@ function addProject() {
   newProject.addEventListener("click", enableProjectListener, true);
   ulProjects.appendChild(newProject);
   clear(input);
+}
+function deleteProject() {
+  const currentProject = document.querySelector(".current").textContent;
+  const defaultProject = document.querySelector(".default-project");
+  const warning = document.querySelector(".warning-wrapper");
+  const targetLi = document.querySelector(`.${currentProject}`);
+  targetLi.style.display = "none";
+  warning.classList.remove("warning-wrapper-active");
+  clearKey(currentProject);
+
+  defaultProject.click();
 }
 // adds the eventlistener, assigns active on click and sets the .current header
 function enableProjectListener() {
@@ -109,9 +145,19 @@ function sideNavInit() {
   if (getLocalStorageKeys().length < 1) return;
   getLocalStorageKeys().forEach((key) => {
     if (key === "Tasks") return;
-    const projectLi = create("li", "project");
+    const projectLi = create("li", "project", `${key}`);
     projectLi.textContent = key;
     projectLi.addEventListener("click", enableProjectListener, true);
     ulProjects.appendChild(projectLi);
   });
+}
+// toggles the warning for deleting a project and the associated tasks
+function toggleWarning() {
+  const currentProject = document.querySelector(".current").textContent;
+  const warning = document.querySelector(".warning-wrapper");
+  const warn = document.querySelector(".warn-item");
+  if (currentProject === "Tasks") return;
+  if (currentProject === "Search Result") return;
+  warn.textContent = currentProject;
+  warning.classList.toggle("warning-wrapper-active");
 }
