6af8dd1fe46a57b08af96a1d955958fffbb75743
georgiosan <georgiosanagnostou@hotmail.com>
2021-10-08 21:41:39 +0300

    Setting output to prod

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-  mode: "development",
+  mode: "production",
   entry: {
     main: "./src/index.js",
   },
